aecb724c26ba5e39689b64fec814cd0a97eab1c0
Mettcon <buckelflips69@web.de>
2024-04-26 10:56:57 +0200

    Course complete Code but Segfault

--- main.cpp
+++ main.cpp
@@ -16,18 +16,30 @@ int main()
     Vector2 mapPosition{ 0.0f, 0.0f };
     const float mapScale = 4.0f;
 
-
+    Texture2D enemyIdle = LoadTexture("characters/goblin_idle_spritesheet.png");
+    Texture2D enemyRun = LoadTexture("characters/goblin_run_spritesheet.png");
     Character player(windowWidth, windowHeight);
 
     Prop props[2]{
         Prop{Vector2{600.0f, 300.0f}, LoadTexture("nature_tileset/Rock.png")},
         Prop{Vector2{400.0f, 500.0f}, LoadTexture("nature_tileset/Log.png")} };
 
+
+
+    Enemy enemies[]{
+        Enemy{Vector2{}, enemyIdle, enemyRun},
+        Enemy{Vector2{windowWidth, windowHeight}, enemyIdle, enemyRun},
+        Enemy{Vector2{175, 175}, LoadTexture("characters/slime_idle_spritesheet.png"), LoadTexture("characters/goblin_run_spritesheet.png")},
+    };
+/*
     Enemy goblin{
         Vector2{},
-        LoadTexture("characters/goblin_idle_spritesheet.png"),
-        LoadTexture("characters/goblin_run_spritesheet.png") };
-    goblin.setTarget(&player);
+        enemyIdle,
+        enemyRun
+    };
+*/
+    //goblin.setTarget(&player);
+    for (auto entry : enemies) entry.setTarget(&player);
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -75,12 +87,19 @@ int main()
             entry.Render(position);
         }
 
-        goblin.tick(GetFrameTime());
+        //goblin.tick(GetFrameTime());
+        for (auto entry : enemies) entry.tick(GetFrameTime());
 
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
         {
-            if (CheckCollisionRecs(goblin.getCollisionRec(), player.getWeaponCollisionRec()))
-                goblin.setAlive(false);
+            //if (CheckCollisionRecs(goblin.getCollisionRec(), player.getWeaponCollisionRec()))
+            //    goblin.setAlive(false);
+
+            for (auto entry : enemies)
+            {
+                if (CheckCollisionRecs(entry.getCollisionRec(), player.getWeaponCollisionRec()))
+                    entry.setAlive(false);
+            }
         }
 
         EndDrawing();
